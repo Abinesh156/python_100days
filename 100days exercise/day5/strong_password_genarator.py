3447d60eb5acd2c69dcf6fa76a663d05c4b10289
Abinesh <s.abinesh@gmail.com>
2025-05-25 23:21:36 +0530

    pass gen success

--- 100days exercise/day5/strong_password_genarator.py
+++ 100days exercise/day5/strong_password_genarator.py
@@ -1,5 +1,7 @@
 import random
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+from typing import final
+
+numbers = ["0", '1', '2', '3', '4', '5', '6', '7', '8','9']
 letter=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 special_characters = [
@@ -8,11 +10,30 @@ special_characters = [
     '/', '?', '`', '~'
 ]
 
-len=int(input("enter the lenth of the password"))
-pass_letter=int(input("enter the number of letter in password"))
-pass_spec=int(input("enter the number of spec in password"))
-passlet=0
+pass_letter=int(input("enter the number of letter in password :"))
+pass_spec1=int(input("enter the number of spec in password :"))
+pass_num1=int(input("enter the number of number in password :"))
+pas_let=""
+pass_spec=""
+pass_num=""
+
 for i in range (0,pass_letter):
-    random1=random.randint(0,int(pass_letter))
-    passlet=letter[random1]
-    print(passlet)
+    ranchar=random.choice(letter)#for letter
+    pas_let+=ranchar
+
+for i in range(0,pass_spec1):
+    ranspec=random.choice(special_characters)#for special char
+    pass_spec+=ranspec
+
+for i in range(0,pass_num1):
+    rannum=random.choice(numbers)
+    pass_num+=rannum
+
+
+total=pas_let+pass_spec+str(pass_num)
+word=list(total)
+random.shuffle(word)
+password=""
+for char in word:
+     password+=char
+print(f"Your very strong password with {pass_letter} numbers, {pass_num1} letters, {pass_spec1} special_character is  {password}")
