73f56ad99eb66df7394eb2d713f9b40300a6d533
abinesh <abinesh.s@althisolutions.com>
2025-07-23 08:47:34 +0530

    functions check

--- 100days exercise/day8/primenumberchecker.py
+++ 100days exercise/day8/primenumberchecker.py
@@ -1,4 +1,12 @@
-num=int(input("enter a number"))
 def primechecker(num):
-    if 1%num:
-        print()
+    if num <= 1:
+        print("Not a prime number")
+        return
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not a prime number")
+            return
+    print("Prime number")
+
+num = int(input("Enter a number: "))
+primechecker(num)
