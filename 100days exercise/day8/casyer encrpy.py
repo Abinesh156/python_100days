d71b9de59ac0d8e67e320d731dde21263d8fa6bd
abinesh <abinesh.s@althisolutions.com>
2025-07-23 10:58:47 +0530

    encode decode

--- 100days exercise/day8/casyer encrpy.py
+++ 100days exercise/day8/casyer encrpy.py
@@ -1,2 +1,36 @@
-text=input("enter the word to encrpt")
-shift=int(input("enter the number fo shift"))
+
+def encrypt(plain_text, shift_amount):
+    cipher_test=""
+    for letter in plain_text:
+         position=alphabet.index(letter)
+         newposition=position+shift_amount
+         newletter=alphabet[newposition]
+         cipher_test+=newletter
+    print(cipher_test)
+    return cipher_test
+
+def decrypt(plain_test,shift_amount):
+    decrpyt_word=""
+    for letter in plain_test:
+        position=alphabet.index(letter)
+        newposition=position-shift_amount
+        newletter=alphabet[newposition]
+        decrpyt_word+=newletter
+    print(decrpyt_word)
+
+while True:
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input(f"Type your message to {direction}:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if direction == "encode":
+        encrypt(text, shift)
+    elif direction == "decode":
+        decrypt(text, shift)
+    else:print("invaild enter encode or decode")
+
+
