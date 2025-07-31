41f8e0ea57dd945aba6f7f653cbea87676d87ec2
abinesh <abinesh.s@althisolutions.com>
2025-07-31 11:33:11 +0530

    Grading program

---
+++ 100days exercise/day9/Grading Program.py
@@ -0,0 +1,20 @@
+students={
+    "harish":70,
+    "Elen":91,
+    "found":22,
+    "bond":74,
+    "starwin":58,
+    "Flash":78
+}
+for grade in students:
+    num=students[grade]
+    if num > 90:
+        students[grade]= "Outstanding"
+    elif num >= 81:
+        students[grade]=" Exceeds Expections"
+    elif num >=71:
+        students[grade]= "Acceptable"
+    else:
+        students[grade]= "Fail"
+
+print(students)
