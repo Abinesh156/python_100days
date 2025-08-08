1b1ca1c98aeb7e27c181ddfb95075b6176ba8c51
abinesh <abinesh.s@althisolutions.com>
2025-08-08 11:43:32 +0530

    day 10 of python

---
+++ 100days exercise/day10/calculator.py
@@ -0,0 +1,28 @@
+#calculator app
+
+# add function
+def add(num1,num2):
+    result=num1+num2
+    return result
+
+#subraction function
+def sub(num1,num2):
+    result=num1-num2
+    return result
+
+#multipication
+def mul(num1,num2):
+    result=num1*num2
+    return result
+
+#division function
+def div(num1,num2):
+    result=num1/num2
+    return result
+
+
+# num_one=input("enter a number to calculate")
+ print(add(1,2))
+
+
+
--- 100days exercise/day9/secretbid.py
+++ 100days exercise/day9/secretbid.py
@@ -1,19 +1,25 @@
+bids = []
 
-bids={}
-end=True
-while end:
-    print("Welcome to the secret bid auction")
-    name=input("Enter your name :")
-    bid_amount=input("Enter the Amount of bid :")
-    # end=False
-    bids[name] = int(bid_amount)
+print("Welcome to the secret bid auction")
 
-    more=input("enter yes or no if any biders left ?")
+def secret_bid(user, amount):
+    temp = {
+        "name": user,
+        "bid_amount": amount,
+    }
+    bids.append(temp)
+
+while True:
+    name = input("Enter your name: ")
+    bid_amount = int(input("Enter the Amount of bid: "))
+    secret_bid(name, bid_amount)
+    more = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
     if more == "no":
-        end =False
-winner=max(bids)
+        break
 
-print(f"the winner is {winner},the amount you bid is {bids[winner]}")
-# print(type(bids))
-
-
+# Find the winner
+if bids:
+    winner = max(bids, key=lambda x: x["bid_amount"])
+    print(f"The winner is {winner['name']}, the amount you bid is {winner['bid_amount']}")
+else:
+    print("No bids were placed.")
