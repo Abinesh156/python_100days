1ecc2ee8ff0cccca4d10dcdaeca22ada68cff8d3
abinesh <abinesh.s@althisolutions.com>
2025-08-11 16:01:40 +0530

    calculator

--- 100days exercise/day10/calculator.py
+++ 100days exercise/day10/calculator.py
@@ -1,28 +1,55 @@
 #calculator app
 
 # add function
-def add(num1,num2):
-    result=num1+num2
-    return result
+def add(n1,n2):
+    return n1+n2
+
 
 #subraction function
-def sub(num1,num2):
-    result=num1-num2
-    return result
+def sub(n1,n2):
+    return n1-n2
+
 
 #multipication
-def mul(num1,num2):
-    result=num1*num2
-    return result
+def mul(n1,n2):
+    return n1*n2
+
 
 #division function
-def div(num1,num2):
-    result=num1/num2
-    return result
+def div(n1,n2):
+    return n1/n2
 
 
-# num_one=input("enter a number to calculate")
- print(add(1,2))
+opration={
+    "+":add,
+    "-":sub,
+    "*":mul,
+    "/":div
+}
+
+num_1=float(input("enter a number to calculate :"))
 
 
+for opt in opration:
+    print(opt)
 
+user_op=input("choose the opration you like to do :")
+calculator=opration[user_op]
+num_2=float(input("enter a second number to calculate :"))
+answer=calculator(num_1,num_2)
+
+print(f"{num_1} {user_op} {num_2} :{answer}")
+#calculation second
+while True:
+    end = input("press Enter to continue calculating type n to end :")
+    if end == "n":
+        break
+    for opt in opration:
+        print(opt)
+    user_op=input(f"choose the opration you like to do {answer}:")
+    num_1=float(input(f"enter a number to calculate with {answer} {user_op}:"))
+    answer1=answer
+    calculator=opration[user_op]
+    answer=calculator(answer,num_1)
+
+    print(f"{answer1} {user_op} {num_1} :{answer}")
