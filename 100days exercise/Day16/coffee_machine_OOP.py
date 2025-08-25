ea9577465a976266151597f31e9d48e935970a1c
abinesh <abinesh.s@althisolutions.com>
2025-08-25 11:14:15 +0530

    coffee machine

--- 100days exercise/Day16/coffee_machine_OOP.py
+++ 100days exercise/Day16/coffee_machine_OOP.py
@@ -1,7 +1,23 @@
-from operator import getitem
-
-from Day16.menu import MenuItem
-from menu import Menu
+from menu import Menu,MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+is_on=True
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
 print("Welcome to Coffee Machine!")
+while is_on:
+    option=menu.get_items()
+    choices=input(f"What would you like? {option} :")
+    if choices=="off":
+        is_on=False
+    elif choices=="report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink=menu.find_drink(choices)
+        if coffee_maker.is_resource_sufficient(drink)and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
